3d45c404e4bfaccbdf0d7efa46919edf4554ed4a
MUmairqureshi <67610498+MUmairqureshi@users.noreply.github.com>
2024-09-15 15:40:55 +0500

    update

--- src/UserRoutes/index.js
+++ src/UserRoutes/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 function UserRoutes() {
   return (
-   <BrowserRouter basename='/Coupon'>
+   <BrowserRouter basename='/'>
     <Routes>
         <Route path="/" element={<Home/>}> </Route>
     </Routes>
